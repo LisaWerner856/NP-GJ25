9db148e70db708de9f94cea7b86e97b3aed904d5
MarijnLubbers <mlubbers5@st.noorderpoort.nl>
2025-02-11 14:28:38 +0100

    Pause/PlayButton
    working buttons

--- GameJam25/Assets/MovementSystem/PlayerMovement.cs
+++ GameJam25/Assets/MovementSystem/PlayerMovement.cs
@@ -5,14 +5,31 @@ public class PlayerWaypointMovement : MonoBehaviour
     [SerializeField] private Transform[] waypoints;
     [SerializeField] private float speed = 2f;
     private int currentWaypointIndex = 0;
+    public float currentSpeed;
+
+    void Start()
+    {
+        currentSpeed = speed;
+    }
+
+    public void OnPausePress()
+    {
+        currentSpeed = 0f;
+    }
+
+    public void OnPlayPress()
+    {
+        currentSpeed = speed;
+    }  
 
     void Update()
     {
+        
         if (waypoints.Length == 0) return;
 
         Transform targetWaypoint = waypoints[currentWaypointIndex];
 
-        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, currentSpeed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, targetWaypoint.position) < 0.1f)
         {
@@ -23,4 +40,5 @@ public class PlayerWaypointMovement : MonoBehaviour
             }
         }
     }
+
 }
