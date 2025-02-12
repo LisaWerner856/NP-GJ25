c69fd84929b1d36d6a857fe5c1ff9a366e529a75
Lisa Werner <lwerner@st.noorderpoort.nl>
2025-02-12 11:14:34 +0100

    Cards can be clicked on from the card tray ui, tile preview will spawn and follow mouse, tile can be placed

--- GameJam25/Assets/Lisa/Scripts/BuildManager.cs
+++ GameJam25/Assets/Lisa/Scripts/BuildManager.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Tilemaps;
-using TMPro;
-using Unity.VisualScripting;
+using UnityEngine.UIElements;
 
 public class BuildManager : MonoBehaviour
 {
@@ -14,6 +12,7 @@ public class BuildManager : MonoBehaviour
     public int selectedCard = 0; // this should be based on the cards in the inventory later.
 
     public Transform cardGridUI;
+    public GameObject tilePreview;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,57 +24,84 @@ public class BuildManager : MonoBehaviour
             UICard.transform.SetParent(cardGridUI);
             UICard.transform.localScale = new Vector3(1f, 1f, 1f);
 
-            // Set the selected card scale to be 1,5 times the size
-            if (i == selectedCard)
-            {
-                UICard.transform.localScale = new Vector3(1.5f, 1.5f, 1f);
-            }
-
             UICards.Add(UICard);
 
             i++;
-
         }
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (tilePreview != null)
         {
-            selectedCard = 0;
-            RenterUITiles();
+            tilePreview.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            tilePreview.transform.position = new Vector3(tilePreview.transform.position.x, tilePreview.transform.position.y, 0f);
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Pressed left mouse button");
+                Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        } else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            selectedCard = 1;
-            RenterUITiles();
-        }
+                tilemap.SetTile(tilemap.WorldToCell(position), cards[selectedCard].GetComponent<BuildingCard>().buildingTile);
 
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            tilemap.SetTile(tilemap.WorldToCell(position), cards[selectedCard].GetComponent<BuildingCard>().buildingTile);
-        }
-        if (Input.GetMouseButton(1))
-        {
-            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            tilemap.SetTile(tilemap.WorldToCell(position), null);
+                // Remove the card from the inventory and update UI
+                RemoveCardFromList(selectedCard);
+
+                // Remove the tile from the preview.
+                Destroy(tilePreview);
+            }
         }
     }
-
-    public void RenterUITiles()
+    public void RenderUITiles()
     {
         int i = 0;
         foreach (GameObject tile in UICards)
         {
             tile.transform.localScale = new Vector3(1f, 1f, 1f);
+            i++;
+        }
+    }
+    public void PreviewCard(Vector3 position)
+    {
+        // Create the tile preview
+        tilePreview = new GameObject("TilePreview");
+        tilePreview.transform.position = position;
 
-            // Set the selected card scale to be 1,5 times the size
-            if (i == selectedCard)
+        // Set up the tile preview sprite renderer (optional)
+        SpriteRenderer renderer = tilePreview.AddComponent<SpriteRenderer>();
+        renderer.sprite = cards[selectedCard].GetComponent<BuildingCard>().buildingTile.sprite;  // Assuming buildingTile has a sprite
+    }
+
+    public void PlaceCard()
+    {
+        if(tilePreview == null)
+        {
+            PreviewCard(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+
+       
+    }
+
+    public void RemoveCardFromList(int cardIndex)
+    {
+        // Remove the card from both the cards list and the UI list
+        Debug.Log($"{cardIndex} cardindex | {cards.Count} cardsCount | {UICards.Count} uiCardsCount");
+        if (cardIndex >= 0 && cardIndex < cards.Count)
+        {
+            cards.RemoveAt(cardIndex);
+
+            Destroy(UICards[cardIndex]);
+            UICards.RemoveAt(cardIndex);
+
+            Debug.Log(cardIndex);
+
+            if (selectedCard >= cards.Count)
             {
-                tile.transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+                selectedCard = cards.Count - 1; // Ensure selectedCard is within bounds
+                if (selectedCard < 0) selectedCard = 0; // If no cards are left, reset to 0
             }
 
-            i++;
+
+            RenderUITiles();
         }
     }
 }
--- GameJam25/Assets/Lisa/Scripts/BuildingCard.cs
+++ GameJam25/Assets/Lisa/Scripts/BuildingCard.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Tilemaps;
+using Unity.VisualScripting;
 
 [System.Serializable]
 public class BuildingCard : MonoBehaviour
 {
+    // Reference to the build manager
+    public BuildManager bm;
+
     public TMP_Text buildingName;
 
     public Image image;
@@ -17,10 +21,24 @@ public class BuildingCard : MonoBehaviour
 
     public Tile buildingTile;
 
+    private void Awake()
+    {
+        bm = GameObject.Find("BuildManager").GetComponent<BuildManager>();
+    }
     private void Start()
     {
         image.sprite = buildingCardSO.buildingImage;
         buildingName.text = buildingCardSO.buildingName;
+    }
+
+    public void OnCardClick()
+    {
+        Debug.Log($"Button clicked! and {bm.tilePreview} ");
+
+        //if (bm.tilePreview != null)
+        //{
+            bm.PlaceCard();
+        //}
     }
 }
 
