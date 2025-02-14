c6476387fe39c7296572df856dca255a1dc47103
Lisa Werner <lwerner@st.noorderpoort.nl>
2025-02-14 01:04:44 +0100

    Modified the spawn position to prevent enemies to spawn too close to the base.

--- GameJam25/Assets/Lisa/Scripts/RoundManager.cs
+++ GameJam25/Assets/Lisa/Scripts/RoundManager.cs
@@ -7,13 +7,19 @@ public class RoundManager : MonoBehaviour
     public Canvas canvas;
     public List<GameObject> enemies = new List<GameObject>(); 
     public List<Transform> possibleSpawnLocations = new List<Transform>();
+    [SerializeField] private int spawnBuffer = 2; // Add this spawn buffer, so enemies don't spawn too close to the base.
+
+    private void Start()
+    {
+        StartNewRound();
+    }
 
     public void StartNewRound()
     {
         Debug.Log("New Round Started!");
         // Spawn enemies, increase their level
         int rndEnemy = Random.Range(0, enemies.Count - 1);
-        int rndSpawnPos = Random.Range(0, possibleSpawnLocations.Count - 1);
+        int rndSpawnPos = Random.Range(0 + spawnBuffer, possibleSpawnLocations.Count - 1);
 
         //Instantiate(enemies[rndEnemy], possibleSpawnLocations[rndEnemy].position, Quaternion.identity);
         GameObject newEnemy = Instantiate(enemies[rndEnemy], canvas.transform);
